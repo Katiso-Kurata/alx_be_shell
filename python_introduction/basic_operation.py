33446cd503d8bbe79b3fd85bb5b3d96a15f21d83
Katiso-Kurata <katisokurata@gmail.com>
2024-06-27 11:24:36 +0000

    First draft

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,11 @@
+# assigning values
+number1 = 10
+number2 = 5
+#performing operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+#print results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {addition}")
+print(f"Multiplicaton of {number1} and {number2} is {multiplication}")
