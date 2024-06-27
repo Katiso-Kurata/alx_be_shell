33d690aa35832c8e5d82fb0617eeef83449d9821
Katiso-Kurata <katisokurata@gmail.com>
2024-06-27 11:57:11 +0000

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#assigning values
+principal = 1000
+rate = 0.05
+time = 3
+#performing calculation
+interest = principal * rate * time
+#printing results
+print(f"The simple interest is: {interest}")
